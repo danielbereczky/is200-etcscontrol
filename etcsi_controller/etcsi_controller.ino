74ffc5b782617d6fcc74629187ef38c3a6e8a6fc
Bereczky Dániel <bereczkydaniel@edu.bme.hu>
2025-05-08 13:55:18 +0200

    add functionality to read throttle pedal and throttle body valve input

--- etcsi_controller/etcsi_controller.ino
+++ etcsi_controller/etcsi_controller.ino
@@ -4,18 +4,24 @@
 //pin assignment
 
   //sensor inputs (redundant)
-  int pedalPin = A0;
+  int pedalPin = A0; // pin for throttle pedal position sensor 1 (rising voltage)
   int pedalPin2 = A1;
 
-  int throttlePin = A2;
+  int throttlePin = A2; // pin for throttle valve position sensor 1 (rising voltage)
   int throttlePin2 = A3;
   //outputs
-  int throttleClutchPin = 0; // motor clutch, should be engaged all the time pretty much
-  int throttleMotorPin = 0; // the throttle body motor itself
-
+  //int throttleClutchPin = 0; // motor clutch, should be engaged all the time pretty much
+  //int throttleMotorPin = 0; // the throttle body motor itself
 
   //variables
   long pedalVal = 0;
+  int pedalValNormalized = 0;
+  int pedalValZeroOffset = 430;
+
+  long throttleVal = 0;
+  int throttleValNormalized = 0;
+  int throttleValZeroOffset = 506;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
@@ -25,25 +31,47 @@ void setup() {
 
 void readVPA(){
   //read the throttle pedal 256 times and average the values, so noise does not mess up the readings.
+  pedalVal = 0;
   for(unsigned int i = 0; i < 256;i++){
     pedalVal += analogRead(pedalPin);
   }
-  pedalVal = pedalVal / 256;
+  pedalVal /=  256;
+  pedalValNormalized = map(pedalVal,pedalValZeroOffset,1023,0,100);
 }
 
-void readVTA();
+void readVTA(){
+    //read the throttle pedal 256 times and average the values, so noise does not mess up the readings.
+  throttleVal = 0;
+  for(unsigned int i = 0; i < 256;i++){
+    throttleVal += analogRead(throttlePin);
+  }
+  throttleVal /=  256;
+  throttleValNormalized = map(throttleVal,throttleValZeroOffset,1023,0,100);
+}
 
 void calculateMotorPWM();
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // basic loop: read sensor target, calculate target write value using a PID algorithm, and then drive the motor with a PWM signal
+  // basic loop: read pedal input, read throttle position, calculate target, write value using a PID algorithm, and drive the motor with a PWM signal
+  //read pedal input
   readVPA();
-  Serial.write(pedalVal);
+  //read throttle input
+  readVTA();
+
+  //ONLY USED FOR DEBUG
+  Serial.print("Throttle Pedal percentage:  ");
+  Serial.print(pedalValNormalized);
+  Serial.print("  Throttle Valve percentage:  ");
+  Serial.print(throttleValNormalized);
+  Serial.print('\n');
+
+
+//breather
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
+  delay(100);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+  delay(100);
 
 }
 
